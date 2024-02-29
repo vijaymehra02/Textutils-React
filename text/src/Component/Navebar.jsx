3fcdbeb04dca41caf722e8ef460162916ece6a1d
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2024-03-01 00:39:11 +0530

    add tolower & cleartext & copytext & removeextraspace feature

--- text/src/Component/Navebar.jsx
+++ text/src/Component/Navebar.jsx
@@ -1,28 +1,54 @@
 
-import React from 'react'
+import React , {useState} from 'react'
 
 export default function Navebar(props) {
+
+    const [modepera , setmodepera] = useState('Enable Dark Mode');
+    const [mode , setmode] = useState('disabled');
+    const [modetoggle , setmodetoggle] = useState({
+        color : 'black',
+        backgroundColor : 'white',
+    });
+
+    const toggle = () =>{
+
+        if(mode === 'disabled'){
+            setmode('checked');
+            setmodepera('Enable Light Mode');
+            setmodetoggle({
+                color : 'white',
+                backgroundColor : 'black',
+            })
+        }else{
+            setmode('disabled');
+            setmodepera('Enable Dark Mode');
+            setmodetoggle({
+                color : 'black',
+                backgroundColor : 'white',
+            })
+        }
+
+    }
+
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg " style={modetoggle}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">{props.tital}</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <a className="navbar-brand" style={modetoggle} href="#">{props.tital}</a>
+                   
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" style={modetoggle} aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" style={modetoggle} aria-current="page" href="#">Home</a>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                        <div className="form-check form-switch">
+                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" onClick={toggle} />
+                            <label className="form-check-label" htmlFor="flexSwitchCheckChecked"  >{modepera}</label>
+                        </div>
                     </div>
                 </div>
             </nav>
--- text/src/Component/Textarea.jsx
+++ text/src/Component/Textarea.jsx
@@ -2,10 +2,24 @@ import React, { useState } from 'react'
 
 export default function Textarea(props) {
 
-    const [text , setText] = useState('Enter your text here');
+    const [text , setText] = useState('');
     function toupper() {
        let temp = text.toUpperCase();
        setText(temp);
+    }
+    function tolower() {
+       let temp = text.toLowerCase();
+       setText(temp);
+    }
+    function cleartext() {
+       setText("");
+    }
+    function copytext() {
+      navigator.clipboard.writeText(text);
+    }
+    function removeextraspace() {
+      let temp = text.split(/[ ]+/);
+      setText(temp.join(" "));
     }
 
     const handleOnClick = (event) => {
@@ -13,16 +27,26 @@ export default function Textarea(props) {
     }
 
     return (
+        <>
         <div>
             <div className='container mt-5'>
 
                     <h2> Text Area TRY IT </h2>
-                    <textarea className="form-control" value={text} onChange={handleOnClick} rows="8" ></textarea>
-                    <button className='btn btn-primary mt-1' onClick={toupper}>convert to uppercase</button>
+                    <textarea className="form-control" value={text} onChange={handleOnClick} placeholder='Enter your text here' rows="8" ></textarea>
+                    <button className='btn btn-primary mt-1 mx-2 my-2' onClick={toupper}>convert to uppercase</button>
+                    <button className='btn btn-primary mt-1 mx-2 my-2' onClick={tolower}>convert to uppercase</button>
+                    <button className='btn btn-primary mt-1 mx-2 my-2' onClick={cleartext}>clear text</button>
+                    <button className='btn btn-primary mt-1 mx-2 my-2' onClick={copytext}>copy text</button>
+                    <button className='btn btn-primary mt-1 mx-2 my-2' onClick={removeextraspace}>Remove extra spaces</button>
               
             </div>
 
         </div>
+        <div className="container">
+            <h1>Your Text Summery</h1>
+            <p>Words : {text.split(" ").length} And Latter : {text.length}</p>
+        </div>
+        </>
 
     )
 }
